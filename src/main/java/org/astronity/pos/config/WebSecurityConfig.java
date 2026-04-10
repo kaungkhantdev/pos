29994b1a64860ca4f62b1fcd2d77b84df8c6ddc2
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2026-04-10 17:51:31 +0700

    add design and test

---
+++ src/main/java/org/astronity/pos/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package org.astronity.pos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.authorizeHttpRequests((requests) -> requests
+                // Public routes - no authentication required
+                .requestMatchers("/", "/home").permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+}
---
+++ src/main/java/org/astronity/pos/controller/HomeController.java
@@ -0,0 +1,19 @@
+package org.astronity.pos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "pos/home";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        return "pos/hello";
+    }
+}
